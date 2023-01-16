2442d0c47b51b033f729d4cc1441a620b1f7e879
Jiahao Fu <2857223287@qq.com>
2023-01-16 14:38:09 +0800

    add top bar content in header

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,24 +1,55 @@
 <template>
     <dev class="header-container">
-        <div class="l-context">
-            1
+        <div class="l-content">
+            <el-button icon="el-icon-menu" size="medium" style="margin-left:20px;"></el-button>
+            <span class="firstPage">First Page</span>
         </div>
-        <div class="r-context">
-            2
+        <div class="r-content">
+            <el-dropdown>
+                <span class="el-dropdown-link">
+                    <img class="user_img" src="@/assets/images/user.jpg" />
+                </span>
+                <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>个人信息</el-dropdown-item>
+                    <el-dropdown-item>注销</el-dropdown-item>
+                </el-dropdown-menu>
+            </el-dropdown>
         </div>
     </dev>
 </template>
 
 <script>
-export default{
-    name:'Header',
-    data(){
-        return{}
+export default {
+    name: 'Header',
+    data() {
+        return {}
     }
 }</script>
 
 <style lang="less" scoped>
-.head-container{
+.head-container {
     height: 60px;
     background-color: #242f42;
-}</style>
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .firstPage {
+        color: #fff;
+        font-size: 16px;
+        margin-left: 10px;
+        cursor: pointer;
+    }
+
+    .r-content {
+        padding-right: 20px;
+
+        .user_img {
+            width: 40px;
+            height: 40px;
+            border-radius: 50%;
+        }
+
+    }
+}
+</style>
