784a6a5dbb174442f1f1013c9da376066031bfe9
Jiahao Fu <2857223287@qq.com>
2023-01-16 17:05:49 +0800

    create table setting js

---
+++ src/store/tab.js
@@ -0,0 +1,11 @@
+export default{
+    state:{
+        // Control menu state
+        isCollapse:false
+    },
+    mutations:{
+        collapseChange(state){
+            state.isCollapse = !state.collapseChange
+        }
+    }
+}
