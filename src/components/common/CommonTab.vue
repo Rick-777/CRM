18c1603cb9cac25e5025fc445210973d88504f66
Jiahao Fu <2857223287@qq.com>
2023-01-25 14:13:15 +0800

    add highlighter to top bar

--- src/components/common/CommonTab.vue
+++ src/components/common/CommonTab.vue
@@ -1,20 +1,42 @@
 <template>
-    <div>
-        <el-tag v-for="item in tags" :key="item.path" effect="dark">
+    <div class="tabs">
+        <el-tag 
+        v-for="item in tags" 
+        :key="item.path" 
+        :effect="$router.name === item.name ? 'dark' : 'plain'"
+        >
             {{ item.label }}
         </el-tag>
     </div>
 </template>
 <script>
-import { mapState,mapMutations } from 'vuex';
+import { mapState, mapMutations } from 'vuex';
 export default {
     name: 'commonTab',
     dara() {
         return {}
-    },computed:{
+    }, computed: {
         ...mapState({
-            tags:state=>state.tab.tabsList
-        })
+            tags: (state) => state.tab.tabsList
+        }),
+    },
+};
+</script>
+<style lang="less" scoped>
+.tabs {
+    position: relative;
+    //height: 30px;
+    overflow: hidden;
+    background: #fff;
+    //padding-right: 120px;
+    box-shadow: 0 5px 10px #ddd;
+    padding-bottom: 5px;
+    padding-top: 5px;
+    padding-left: 10px;
+
+    .el-tag {
+        margin-right: 15px;
+        cursor: pointer
     }
 }
-</script>
+</style>
