ce7344b58a42b0fe25532633b79647410e26aff4
Jiahao Fu <2857223287@qq.com>
2023-02-06 00:35:13 +0800

    add menu files

---
+++ Back-End/src/main/java/com/lib/sys/mapper/SysMenuMapper.java
@@ -0,0 +1,16 @@
+package com.lib.sys.mapper;
+
+import com.lib.sys.entity.SysMenu;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 菜单管理 Mapper 接口
+ * </p>
+ *
+ * @author 波哥
+ * @since 2022-11-18
+ */
+public interface SysMenuMapper extends BaseMapper<SysMenu> {
+
+}
---
+++ Back-End/src/main/java/com/lib/sys/service/impl/SysMenuServiceImpl.java
@@ -0,0 +1,20 @@
+package com.lib.sys.service.impl;
+
+import com.lib.sys.entity.SysMenu;
+import com.lib.sys.mapper.SysMenuMapper;
+import com.lib.sys.service.ISysMenuService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 菜单管理 服务实现类
+ * </p>
+ *
+ * @author 波哥
+ * @since 2022-11-18
+ */
+@Service
+public class SysMenuServiceImpl extends ServiceImpl<SysMenuMapper, SysMenu> implements ISysMenuService {
+
+}
